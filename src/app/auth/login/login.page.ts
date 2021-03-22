e98a82f1238c5a35894e4f6ce637503610b7078d
Lucas Avila <lucas_avila@div-it.com.ar>
2021-03-21 23:40:56 -0300

    inicio

--- src/app/auth/login/login.page.ts
+++ src/app/auth/login/login.page.ts
@@ -2,6 +2,9 @@ import { Route } from '@angular/compiler/src/core';
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { AuthService } from '../auth.service';
+import { FormGroup, FormBuilder, Validators } from "@angular/forms";
+import { AlertController } from '@ionic/angular';
+
 
 @Component({
   selector: 'app-login',
@@ -10,21 +13,55 @@ import { AuthService } from '../auth.service';
 })
 export class LoginPage implements OnInit {
 
+  isSubmitted:boolean;
+  ionicForm: FormGroup;
+  alert:AlertController
+
   constructor(
     private authService : AuthService,
-    private router : Router
-  ) { }
-
-  ngOnInit() {
+    private router : Router,
+    public formBuilder: FormBuilder,
+    public alertController: AlertController
+  ) { 
+    this.isSubmitted = false;
   }
 
-  login(form){
-    this.authService.login(form.value).subscribe((res)=>{                        
-      if(res.loguedIn){        
-        this.router.navigateByUrl('tabs/tab1');
-      }
-      alert('Usuario incorrecto');
+  async presentAlert() {
+    const alert = await this.alertController.create({
+      cssClass: 'my-custom-class',
+      header: 'Error de validación',
+      subHeader: 'Revise los datos',
+      message: 'Por favor revise los datos ingresados.',
+      buttons: ['OK']
     });
+
+    await alert.present();
+  }
+  get errorControl() {
+    return this.ionicForm.controls;
+  }
+
+  ngOnInit() {
+    this.isSubmitted = false;
+    this.ionicForm = this.formBuilder.group({
+      email: ['', [Validators.required, Validators.minLength(4), Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(4)]],
+   })
+  }
+
+  login(){
+    console.log(this.ionicForm.value);
+    this.isSubmitted = true;
+    if (!this.ionicForm.valid) {            
+      this.presentAlert();
+      return false;
+    } else {
+      this.authService.login(this.ionicForm.value).subscribe((res)=>{        
+        if(res.loguedIn){        
+          this.router.navigateByUrl('tabs/tab1');
+      }      
+     });
+    }
   }
 
 }
