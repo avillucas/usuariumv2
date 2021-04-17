8e9b916a81fe176d61cc32b5af70baf4250f10ec
Lucas Avila <lucas_avila@div-it.com.ar>
2021-04-16 22:48:28 -0300

    splash screen 1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { Platform } from '@ionic/angular';
+import { Router } from '@angular/router';
+import { Plugins  } from '@capacitor/core';
+const {SplashScreen} = Plugins;
 
 @Component({
   selector: 'app-root',
@@ -6,5 +10,17 @@ import { Component } from '@angular/core';
   styleUrls: ['app.component.scss'],
 })
 export class AppComponent {
-  constructor() {}
+  constructor(
+    private platform: Platform,    
+    private router:Router
+  ) {   
+    this.initialiceApp();
+  }
+
+  initialiceApp(){
+    SplashScreen.hide()
+    this.platform.ready().then(()=>{
+      this.router.navigateByUrl('splash');
+    });
+  }
 }
